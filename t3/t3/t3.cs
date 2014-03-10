ae511f4dd3efd4dc974a18774ff5d779068bd0a4
glenn swonk <glenn@swonk.com>
2014-03-10 16:58:53 -0700

    test 1

---
+++ t3/t3/t3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+
+namespace t3
+{
+    class t3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<t3 rev='First time...'/");
+            Console.WriteLine("<t3 rev='First time...'/");
+            Console.WriteLine("<t3 rev='First time...'/");
+            // 71d81be741a4411e146e83b132dd1c7c8e34b600 
+        }
+    }
+}
+// ////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////
