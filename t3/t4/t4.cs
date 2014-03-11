f29c691f03010ae0f838378b6e2b1a3bdf3bfbd6
glenn swonk <glenn@swonk.com>
2014-03-11 09:36:15 -0700

    qwee

---
+++ t3/t4/t4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+
+namespace t4
+{
+    class t4
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<t4 rev='First time...'/");
+            Console.WriteLine("<t4 rev='First time...'/");
+            Console.WriteLine("<t4 rev='First time...'/");
+        }
+    }
+}
+// ////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////
