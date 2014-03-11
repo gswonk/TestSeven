6b5e53ba8c3bd4f92fd72fef3545480bd6f1015e
glenn swonk <glenn@swonk.com>
2014-03-11 09:35:53 -0700

    qqww

---
+++ t3/t5/t5.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t5
+{
+    class t5
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
